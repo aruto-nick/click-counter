db4685fcd826dbc4c36d7cc529bbad63d1e29420
aruto-nick <effo.conti329@gmail.com>
2026-02-03 18:30:14 +0900

    「クリックカウンター」※同じ処理

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const message = document.getElementById("message");
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btnMessage");
 
-btn.addEventListener("click", () => {
+btnMessage.addEventListener("click", () => {
   message.textContent = "クリックされました！";
+});
+
+const countText = document.getElementById("count");
+const btnCount = document.getElementById("btnCount");
+
+let count = 0;
+
+btnCount.addEventListener("click",()=>{
+  count++;
+  countText.textContent = `クリック回数:${count}`;
 });
