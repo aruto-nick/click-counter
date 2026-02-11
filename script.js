14e67e001d459bd2920443f0048f3e42ea1ffce2
aruto-nick <effo.conti329@gmail.com>
2026-02-11 21:30:51 +0900

    関数設定→初期画面表示

--- script.js
+++ script.js
@@ -11,19 +11,55 @@ const restBtn = document.getElementById("resetBtn");
 
 let count = 0;
 
-btnCount.addEventListener("click",()=>{
-  count++;
 
-  if(count>=10){
+// 関数：まとめ役
+
+function updateDisplay(){
+  updateText();
+  updateButtonState();
+}
+
+// 関数：表示担当
+function updateText (){
+  if (count >= 10){
     countText.textContent = "🎉 10回達成！";
     countText.classList.add("achieved");
-  }else{
-    countText.textContent = `クリック回数:${count}`;  
   }
-  
+
+  else if(count >= 5){
+    countText.textContent = `クリック回数：${count}(もう少し！)`;
+    countText.classList.remove("achieved");
+  }
+
+  else {
+    countText.textContent = `クリック回数：${count}`;
+    countText.classList.remove("achieved");
+  }
+}
+
+// 関数：ボタン担当
+
+function updateButtonState(){
+  if (count >= 10){
+    btnCount.disabled = true;
+  }
+
+  else{
+    btnCount.disabled = false;
+  }
+}
+
+btnCount.addEventListener("click",()=>{
+  count++;
+  updateDisplay();  
 });
+
+
 
 resetBtn.addEventListener("click",()=>{
   count = 0 ;
-  countText.textContent = `クリック回数：${count}`;
+  btnCount.disabled = false ;
+  updateDisplay();
 });
+
+updateDisplay();
